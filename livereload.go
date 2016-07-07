650f9f6bc77a7b99bf13ebf896394b97a4c10262
Simon Epskamp <simon@frontwise.com>
2016-07-07 14:37:39 +0200

    Example copied

--- livereload.go
+++ livereload.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"log"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	startWatching()
+}
+
+func startWatching() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/home/simon/tmp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }

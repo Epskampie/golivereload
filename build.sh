d04f9998830665a02403c315e8e94d94401fbeaf
Simon Epskamp <simon@friendlypixel.nl>
2020-05-07 19:34:59 +0200

    build.sh add macos

--- build.sh
+++ build.sh
@@ -5,12 +5,17 @@ rm -rf build/*
 GOOS=windows GOARCH=amd64 go build -o build/windows_amd64/golivereload.exe
 cd build/windows_amd64
 zip `pwd | xargs basename`.zip *
+mv *.zip ..
 cd -
 
 GOOS=linux GOARCH=amd64 go build -o build/linux_amd64/golivereload
 cd build/linux_amd64
 tar czf `pwd | xargs basename`.tar.gz *
+mv *.tar.gz ..
 cd -
 
-# GOOS=darwin go build -o build/macos/golivereload
-
+GOOS=darwin go build -o build/macos/golivereload
+cd build/macos
+tar czf `pwd | xargs basename`.tar.gz *
+mv *.tar.gz ..
+cd -
